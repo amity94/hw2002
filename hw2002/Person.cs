6095abf2f63a4041eb4185ca8956b316cf9c2bef
amity94 <45635119+amity94@users.noreply.github.com>
2019-02-24 19:01:38 +0200

    Add files via upload

---
+++ hw2002/Person.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw2002
+{
+    class Person : IComparable<Person>
+    {
+        //SETTERS AND GETTERS
+        public int Id { get; private set; }
+        public int Age { get; private set; }
+        public float Height { get; private set; }
+        public string Name { get; private set; }
+
+        private static readonly IComparer<Person> idComparer;
+        private static readonly IComparer<Person> ageComparer;
+        private static readonly IComparer<Person> heightComparer;
+        private static readonly IComparer<Person> nameComparer;
+
+        public static IComparer<Person> IDComparer { get { return idComparer; } }
+        public static IComparer<Person> AgeComparer { get { return ageComparer; } }
+        public static IComparer<Person> HeightComparer { get { return ageComparer; } }
+        public static IComparer<Person> NameComparer { get { return nameComparer; } }
+
+        private static IComparer<Person> DefaultComparer;
+
+
+
+        public Person(int id, int age, float height, string name)
+        {
+            this.Id = id;
+            this.Age = age;
+            this.Height = height;
+            this.Name = name;
+        }
+
+        static Person()
+        {
+            idComparer = new PersonCompareById();
+            ageComparer = new PersonCompareByAge();
+            heightComparer = new PersonCompareByHeight();
+            nameComparer = new PersonCompareByName();
+        }
+
+        public static void ModifyDefaultComparer(string any)
+        {
+           
+        }
+
+
+        //public static IComparer<Person> GetDefaultComparer()
+        //{
+
+        //}
+
+        public override string ToString()
+        {
+            return $"Id: {Id}, Age: {Age}, Height: {Height}, Name: {Name}";
+        }
+
+        public int CompareTo(Person other)
+        {
+            return new PersonCompareById().Compare(this, other);
+        }
+    }
+
+
+}
---
+++ hw2002/PersonCompareByHeight.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw2002
+{
+    class PersonCompareByHeight : IComparer<Person>
+    {
+        public int Compare(Person x, Person y)
+        {
+            if(x.Height > y.Height)
+            {
+                return 1;
+            }
+            if (x.Height == y.Height)
+            {
+                return 0;
+            }
+
+            return -1;
+        }
+    }
+}
